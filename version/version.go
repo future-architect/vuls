938188383524697fc5b9f19a44f81cd8141aaad8
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2016-05-25 09:49:16 +0900

    mv version.go version/version.go to run main.go without compile

--- version/version.go
+++ version/version.go
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-package main
+package version
 
 // Name is Vuls
 const Name string = "vuls"
