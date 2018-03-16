887454bf495debefb0eb64b14fce7eae70833f46
Kota Kanbe <kotakanbe@gmail.com>
2018-03-16 17:33:18 +0900

    Update vulninfos.go

--- models/vulninfos.go
+++ models/vulninfos.go
@@ -269,7 +269,6 @@ func (v VulnInfo) Cvss3Scores() (values []CveContentCvss) {
 
 // MaxCvss3Score returns Max CVSS V3 Score
 func (v VulnInfo) MaxCvss3Score() CveContentCvss {
-	// TODO implement NVD
 	order := []CveContentType{NVDJSON, RedHat, JVN}
 	max := 0.0
 	value := CveContentCvss{
