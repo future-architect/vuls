1bf5698300d73d7acbd10c63dad569f51b7eeef8
Sadayuki Matsuno <sadayuki.matsuno@gmail.com>
2018-08-13 11:34:53 +0900

    ignore other writer if -to-saas

--- commands/report.go
+++ commands/report.go
@@ -332,7 +332,7 @@ func (p *ReportCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}
 		}
 	}
 
-	if (len(reports) != 2 || reports[1] != report.SaasWriter{}) {
+	if c.Conf.ToSaas {
 		util.Log.Info("Validating db config...")
 		if !c.Conf.ValidateOnReportDB() {
 			return subcommands.ExitUsageError
