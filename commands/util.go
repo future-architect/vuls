fe6868c2c934e5c1761c2a3f6cc7f7ec064dc4a0
Iskander Sharipov <quasilyte@gmail.com>
2018-10-03 20:25:23 +0300

    commands: simplify `s[:]` to s
    If s is a slice, then `s[:]` is identical to just `s`.
    
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- commands/util.go
+++ commands/util.go
@@ -34,7 +34,7 @@ func getPasswd(prompt string) (string, error) {
 			return "", fmt.Errorf("Failed to read password")
 		}
 		if 0 < len(pass) {
-			return string(pass[:]), nil
+			return string(pass), nil
 		}
 	}
 
