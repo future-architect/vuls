f4c873a3fff861461aa2a7b1e404037b32218d87
Kinoshita Kazumi <e145702@ie.u-ryukyu.ac.jp>
2019-01-27 00:59:43 +0900

    refactor

--- commands/scan.go
+++ commands/scan.go
@@ -207,7 +207,6 @@ func (p *ScanCmd) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) 
 		util.Log.Errorf("Failed to scan. err: %s", err)
 		return subcommands.ExitFailure
 	}
-
 	fmt.Printf("\n\n\n")
 	fmt.Println("To view the detail, vuls tui is useful.")
 	fmt.Println("To send a report, run vuls report -h.")
