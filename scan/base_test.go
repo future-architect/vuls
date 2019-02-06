b9c976713c39fe8489208939988f4f8c2b446602
Kinoshita Kazumi <e145702@ie.u-ryukyu.ac.jp>
2019-02-06 22:48:23 +0900

    fix: test

--- scan/base_test.go
+++ scan/base_test.go
@@ -190,17 +190,7 @@ func TestContentConvertVinfos(t *testing.T) {
 		expected []models.VulnInfo
 	}{
 		{
-			in1: `{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\"
-		:\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecur
-		e\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.
-		4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\
-		":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04
-		.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type
-		\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.
-		fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://gi
-		thub.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wor
-		dpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\":nu
-		ll}]}}`,
+			in1: "{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\":\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecure\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://github.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wordpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\": null}]}}",
 			in2: WpStatus{Name: "twentyfifteen", Status: "inactive", Update: "available", Version: "1.1"},
 			expected: []models.VulnInfo{
 				{
@@ -246,32 +236,12 @@ func TestContentConvertVinfos(t *testing.T) {
 			},
 		},
 		{
-			in1: `{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\"
-		:\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecur
-		e\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.
-		4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\
-		":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04
-		.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type
-		\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.
-		fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://gi
-		thub.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wor
-		dpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\": "
-		1.0"}]}}`,
+			in1: "{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\":\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecure\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://github.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wordpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\": \"1.0\"}]}}",
 			in2: WpStatus{Name: "twentyfifteen", Status: "inactive", Update: "available", Version: "1.1"},
 			expected: []models.VulnInfo{},
 		},
 		{
-			in1: `{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\"
-		:\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecur
-		e\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.
-		4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\
-		":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04
-		.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type
-		\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.
-		fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://gi
-		thub.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wor
-		dpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\": "
-		1.2"}]}}`,
+			in1: "{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\":\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecure\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://github.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wordpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\": \"1.2\"}]}}",
 			in2: WpStatus{Name: "twentyfifteen", Status: "inactive", Update: "available", Version: "1.1"},
 			expected: []models.VulnInfo{
 				{
@@ -319,7 +289,7 @@ func TestContentConvertVinfos(t *testing.T) {
 	}
 	for _, test := range tests {
 		actual, _ := contentConvertVinfos(test.in1, test.in2)
-		if reflect.ValueOf(test.expected).Pointer() == reflect.ValueOf(actual).Pointer() {
+		if reflect.ValueOf(test.expected).Pointer() != reflect.ValueOf(actual).Pointer() {
 			t.Errorf("expected %v, actual %v", test.expected, actual)
 		}
 	}
@@ -332,17 +302,7 @@ func TestCoreConvertVinfos(t *testing.T) {
 		in1      string
 		expected []models.VulnInfo
 	}{
-		in1: `{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\"
-		:\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecur
-		e\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.
-		4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\
-		":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04
-		.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type
-		\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.
-		fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://gi
-		thub.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wor
-		dpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\":nu
-		ll}]}}`,
+		in1: "{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\":\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecure\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://github.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wordpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\":null}]}}",
 		expected: []models.VulnInfo{
 			{
 				CveID:       "CVE-2018-6389",
@@ -387,7 +347,7 @@ func TestCoreConvertVinfos(t *testing.T) {
 		},
 	}
 	actual, _ := coreConvertVinfos(test.in1)
-	if reflect.ValueOf(test.expected).Pointer() == reflect.ValueOf(actual).Pointer() {
+	if reflect.ValueOf(test.expected).Pointer() != reflect.ValueOf(actual).Pointer() {
 		t.Errorf("expected %v, actual %v", test.expected, actual)
 	}
 
