bc17b5e0904150a369b899b4c9b40f666d0edea8
Kinoshita Kazumi <e145702@ie.u-ryukyu.ac.jp>
2019-02-09 00:49:55 +0900

    refactor

--- scan/base_test.go
+++ scan/base_test.go
@@ -242,4 +242,37 @@ func TestContentConvertVinfos(t *testing.T) {
 			t.Errorf("expected %v, actual %v", k, h)
 		}
 	}
+
+}
+
+func TestCoreConvertVinfos(t *testing.T) {
+
+	var test = struct {
+		in1      string
+		expected []models.VulnInfo
+	}{
+		in1: "{\"4.9.4\":{\"release_date\":\"2018-02-06\",\"changelog_url\":\"https://codex.wordpress.org/Version_4.9.4\",\"status\":\"insecure\",\"vulnerabilities\":[{\"id\":9021,\"title\":\"WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)\",\"created_at\":\"2018-02-05T16:50:40.000Z\",\"updated_at\":\"2018-08-29T19:13:04.000Z\",\"published_date\":\"2018-02-05T00:00:00.000Z\",\"vuln_type\":\"DOS\",\"references\":{\"url\":[\"https://baraktawily.blogspot.fr/2018/02/how-to-dos-29-of-world-wide-websites.html\",\"https://github.com/quitten/doser.py\",\"https://thehackernews.com/2018/02/wordpress-dos-exploit.html\"],\"cve\":[\"2018-6389\"]},\"fixed_in\":null}]}}",
+		expected: []models.VulnInfo{
+			{
+				CveID: "CVE-2018-6389",
+				AffectedPackages: models.PackageStatuses{
+					models.PackageStatus{
+						NotFixedYet: true,
+					},
+				},
+				CveContents: models.NewCveContents(
+					models.CveContent{
+						CveID: "CVE-2018-6389",
+						Title: "WordPress <= 4.9.4 - Application Denial of Service (DoS) (unpatched)",
+					},
+				),
+			},
+		},
+	}
+	actual, _ := coreConvertVinfos(test.in1)
+	if !reflect.DeepEqual(test.expected, actual) {
+		h := pp.Sprint(actual)
+		k := pp.Sprint(test.expected)
+		t.Errorf("expected %v, actual %v", k, h)
+	}
 }
