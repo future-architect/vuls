edc3646f08d9c665c14113320101606b7832c3fc
Kota Kanbe <kotakanbe@gmail.com>
2019-06-28 00:25:14 +0900

    feat(yumps): get affected procs on RHEL6 and RHEL8

--- scan/redhatbase.go
+++ scan/redhatbase.go
@@ -189,6 +189,9 @@ func (o *redhatBase) preCure() error {
 }
 
 func (o *redhatBase) postScan() error {
+	//TODO
+	o.yumPsUshida()
+
 	if o.isExecYumPS() {
 		if err := o.yumPS(); err != nil {
 			err = xerrors.Errorf("Failed to execute yum-ps: %w", err)
@@ -489,7 +492,73 @@ func (o *redhatBase) isExecNeedsRestarting() bool {
 	return true
 }
 
-// How to install yum-plugin-ps on RHEL
+type loadedFile struct {
+	pid      string
+	procName string
+	path     string
+	pkgName  string
+}
+
+func (o *redhatBase) yumPsUshida() error {
+	stdout, err := o.ps()
+	if err != nil {
+		// TODO wrap
+		return err
+	}
+	pidNames := o.parsePs(stdout)
+
+	loadedFiles := []loadedFile{}
+	for pid, name := range pidNames {
+		stdout := ""
+		stdout, err = o.procExe(pid)
+		if err != nil {
+			// TODO wrap
+			return err
+		}
+		s, err := o.parseProcExe(stdout)
+		if err != nil {
+			// TODO wrap or only logging
+			return err
+		}
+		loadedFiles = append(loadedFiles, loadedFile{
+			pid:      pid,
+			procName: name,
+			path:     s,
+		})
+
+		stdout, err = o.procMap(pid)
+		if err != nil {
+			// TODO wrap
+			return err
+		}
+		ss := o.parseProcMap(stdout)
+		for _, s := range ss {
+			loadedFiles = append(loadedFiles, loadedFile{
+				pid:      pid,
+				procName: name,
+				path:     s,
+			})
+		}
+	}
+
+	for i, loaded := range loadedFiles {
+		name, ok, err := o.getPkgName(loaded.path)
+		if err != nil {
+			// TODO wrap
+			return err
+		}
+		if !ok {
+			continue
+		}
+		loaded.pkgName = name
+		loadedFiles[i] = loaded
+	}
+
+	fmt.Println(loadedFiles)
+	return nil
+}
+
+// How to install yum-plugin-ps on RHEL7
 // # yum-config-manager --enable rhui-REGION-rhel-server-optional
 // # yum install yum-plugin-ps
 // # yum-config-manager --disable rhui-REGION-rhel-server-optional
@@ -691,4 +760,73 @@ func (o *redhatBase) hasYumColorOption() bool {
 	cmd := "yum --help | grep color"
 	r := o.exec(util.PrependProxyEnv(cmd), noSudo)
 	return len(r.Stdout) > 0
+}
+
+func (o *redhatBase) ps() (stdout string, err error) {
+	cmd := `ps  --no-headers --ppid 2 -p 2 --deselect -o pid,comm | awk '{print $1,$2}'`
+	// TODO sudo
+	r := o.exec(util.PrependProxyEnv(cmd), o.sudo.yumPS())
+	if !r.isSuccess() {
+		return "", xerrors.Errorf("Failed to SSH: %s", r)
+	}
+	return r.Stdout, nil
+}
+
+func (o *redhatBase) parsePs(stdout string) map[string]string {
+	pidNames := map[string]string{}
+	scanner := bufio.NewScanner(strings.NewReader(stdout))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		ss := strings.Fields(line)
+		if len(ss) < 2 {
+			continue
+		}
+		pidNames[ss[0]] = ss[1]
+	}
+	return pidNames
+}
+
+func (o *redhatBase) procExe(pid string) (stdout string, err error) {
+	cmd := fmt.Sprintf("ls -l /proc/%s/exe", pid)
+	r := o.exec(util.PrependProxyEnv(cmd), sudo)
+	if !r.isSuccess() {
+		return "", xerrors.Errorf("Failed to SSH: %s", r)
+	}
+	return r.Stdout, nil
+}
+
+func (o *redhatBase) parseProcExe(stdout string) (string, error) {
+	ss := strings.Fields(stdout)
+	if len(ss) < 11 {
+		return "", xerrors.Errorf("Unknown format: %s", stdout)
+	}
+	return ss[10], nil
+}
+
+func (o *redhatBase) procMap(pid string) (stdout string, err error) {
+	cmd := fmt.Sprintf(`cat /proc/%s/maps 2>/dev/null | grep -v " 00:00 " | awk '{print $6}' | sort -n | uniq`, pid)
+	r := o.exec(util.PrependProxyEnv(cmd), sudo)
+	if !r.isSuccess() {
+		return "", xerrors.Errorf("Failed to SSH: %s", r)
+	}
+	return r.Stdout, nil
+}
+
+func (o *redhatBase) parseProcMap(stdout string) (soPaths []string) {
+	scanner := bufio.NewScanner(strings.NewReader(stdout))
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		soPaths = append(soPaths, line)
+	}
+	return soPaths
+}
+
+func (o *redhatBase) getPkgName(path string) (pkgName string, found bool, err error) {
+	cmd := fmt.Sprintf(`rpm -qf %s`, path)
+	r := o.exec(util.PrependProxyEnv(cmd), noSudo)
+	if !r.isSuccess() {
+		return "", false, xerrors.Errorf("Failed to SSH: %s", r)
+	}
+	s := strings.TrimSpace(r.Stdout)
+	return s, s != "", nil
 }
