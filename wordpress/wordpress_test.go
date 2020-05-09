e864115783a133d143736e1e72882ba8d47b5fb7
Kazuminn <warugaki.k.k@gmail.com>
2020-05-09 21:14:47 +0900

    add unset test

--- wordpress/wordpress_test.go
+++ wordpress/wordpress_test.go
@@ -33,3 +33,30 @@ func TestFillInactive(t *testing.T) {
 		}
 	}
 }
+
+func TestUnset(t *testing.T) {
+	var tests = []struct {
+		in       models.WordPressPackages
+		expected models.WordPressPackages
+	}{
+		{
+			in: models.WordPressPackages{
+				{
+					Name:    "akismet",
+					Status:  "inactive",
+					Update:  "",
+					Version: "",
+					Type:    "",
+				},
+			},
+			expected: models.WordPressPackages{},
+		},
+	}
+
+	for i, tt := range tests {
+		actual := unset(tt.in, 0)
+		if !reflect.DeepEqual(actual, tt.expected) {
+			t.Errorf("[%d] WordPressPackages error ", i)
+		}
+	}
+}
