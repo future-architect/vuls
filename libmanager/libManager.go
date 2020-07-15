236746bb90c242e314e256cac6f5fc9645964062
Alexander Stein <astein@flexion.us>
2020-07-14 23:18:24 -0400

    More debugging info for Packages struct.

--- libmanager/libManager.go
+++ libmanager/libManager.go
@@ -41,6 +41,7 @@ func FillLibrary(r *models.ScanResult) (totalCnt int, err error) {
 
 	for _, lib := range r.LibraryScanners {
 		for _, l := range lib.Libs {
+			util.Log.Debug(l.Name)
 			r.Packages[l.Name] = models.Package{
 				Name:    l.Name,
 				Version: l.Version,
