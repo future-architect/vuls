65f7cacb10cdac6ce048e6c0c29fc86b7cf1472a
Kota Kanbe <kotakanbe@gmail.com>
2020-09-12 05:06:48 +0900

    fix test

--- models/scanresults_test.go
+++ models/scanresults_test.go
@@ -688,7 +688,7 @@ func TestIsDisplayUpdatableNum(t *testing.T) {
 		{
 			mode:     []byte{config.Fast},
 			family:   config.FreeBSD,
-			expected: true,
+			expected: false,
 		},
 		{
 			mode:     []byte{config.Fast},
