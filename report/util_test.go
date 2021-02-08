597a478f800ae8dd18e511435d303def0e57aae1
kazuminn <warugaki.k.k@gmail.com>
2021-02-08 22:40:45 +0900

    change test & pass

--- report/util_test.go
+++ report/util_test.go
@@ -279,6 +279,7 @@ func TestPlusDiff(t *testing.T) {
 					"CVE-2016-6662": {
 						CveID:            "CVE-2016-6662",
 						AffectedPackages: models.PackageFixStatuses{{Name: "mysql-libs"}},
+						StatusDiff: "+",
 					},
 				},
 				Packages: models.Packages{
@@ -420,6 +421,7 @@ func TestMinusDiff(t *testing.T) {
 					"CVE-2016-6662": {
 						CveID:            "CVE-2016-6662",
 						AffectedPackages: models.PackageFixStatuses{{Name: "mysql-libs"}},
+						StatusDiff: "-",
 					},
 				},
 				Packages: models.Packages{
