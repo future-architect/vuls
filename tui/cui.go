58f61e32f2abb2a46aa66fda98479e6e5c7657e1
MaineK00n <dev.pylori1229@gmail.com>
2021-04-07 08:59:26 +0900

    feat(tui): wip create tui layout

--- tui/cui.go
+++ tui/cui.go
@@ -1,6 +1,9 @@
 package tui
 
 import (
+	"sort"
+
+	"github.com/future-architect/vuls/logging"
 	"github.com/future-architect/vuls/models"
 	"github.com/google/subcommands"
 	"github.com/rivo/tview"
@@ -8,9 +11,26 @@ import (
 
 // RunCui execute main logic
 func RunCui(results models.ScanResults) subcommands.ExitStatus {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+	scanResults = results
+	sort.Slice(scanResults, func(i, j int) bool {
+		if scanResults[i].ServerName == scanResults[j].ServerName {
+			return scanResults[i].Container.Name < scanResults[j].Container.Name
+		}
+		return scanResults[i].ServerName < scanResults[j].ServerName
+	})
+
+	app := tview.NewApplication()
+	flex := tview.NewFlex().
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+				AddItem(tview.NewBox().SetBorder(true).SetTitle("Target"), 20, 1, false).
+				AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+					AddItem(tview.NewBox().SetBorder(true).SetTitle("Summary"), 0, 1, false), 0, 1, false), 0, 1, false).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+				AddItem(tview.NewBox().SetBorder(true).SetTitle("Details"), 0, 1, false), 0, 1, false), 0, 1, false)
+	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
+		logging.Log.Errorf("%+v", err)
+		return subcommands.ExitFailure
 	}
 	return subcommands.ExitSuccess
 }
