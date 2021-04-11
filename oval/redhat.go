641d4c21412e9daf251d0cb27b70fc1ff7b28564
Kota Kanbe <kotakanbe@gmail.com>
2021-04-12 07:09:39 +0900

    fix lint err

--- oval/redhat.go
+++ oval/redhat.go
@@ -33,7 +33,7 @@ func (o RedHatBase) FillWithOval(r *models.ScanResult) (nCVEs int, err error) {
 		}
 		defer func() {
 			if err := driver.CloseDB(); err != nil {
-				logging.Log.Errorf("Failed to close DB. err: %+v")
+				logging.Log.Errorf("Failed to close DB. err: %+v", err)
 			}
 		}()
 
