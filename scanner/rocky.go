f8c490a6a7556c70f45d1fa52d9e4b340842e142
Kota Kanbe <kotakanbe@gmail.com>
2021-07-02 08:02:49 +0900

    refactor: rocky scanner

--- scanner/rocky.go
+++ scanner/rocky.go
@@ -6,10 +6,7 @@ import (
 	"github.com/future-architect/vuls/models"
 )
 
-// inherit OsTypeInterface
-type rocky struct {
-	redhatBase
-}
+type rocky = centos
 
 // NewAmazon is constructor
 func newRocky(c config.ServerInfo) *rocky {
@@ -21,98 +18,10 @@ func newRocky(c config.ServerInfo) *rocky {
 					VulnInfos: models.VulnInfos{},
 				},
 			},
-			sudo: rootPrivRocky{},
+			sudo: rootPrivCentos{},
 		},
 	}
 	r.log = logging.NewNormalLogger()
 	r.setServerInfo(c)
 	return r
-}
-
-func (o *rocky) checkScanMode() error {
-	return nil
-}
-
-func (o *rocky) checkDeps() error {
-	if o.getServerInfo().Mode.IsFast() {
-		return o.execCheckDeps(o.depsFast())
-	} else if o.getServerInfo().Mode.IsFastRoot() {
-		return o.execCheckDeps(o.depsFastRoot())
-	} else {
-		return o.execCheckDeps(o.depsDeep())
-	}
-}
-
-func (o *rocky) depsFast() []string {
-	if o.getServerInfo().Mode.IsOffline() {
-		return []string{}
-	}
-
-	// repoquery
-	// `rpm -qa` shows dnf-utils as yum-utils on RHEL8, CentOS8, Rocky
-	return []string{"yum-utils"}
-}
-
-func (o *rocky) depsFastRoot() []string {
-	if o.getServerInfo().Mode.IsOffline() {
-		return []string{}
-	}
-
-	// repoquery
-	// `rpm -qa` shows dnf-utils as yum-utils on RHEL8, CentOS8, Rocky
-	return []string{"yum-utils"}
-}
-
-func (o *rocky) depsDeep() []string {
-	return o.depsFastRoot()
-}
-
-func (o *rocky) checkIfSudoNoPasswd() error {
-	if o.getServerInfo().Mode.IsFast() {
-		return o.execCheckIfSudoNoPasswd(o.sudoNoPasswdCmdsFast())
-	} else if o.getServerInfo().Mode.IsFastRoot() {
-		return o.execCheckIfSudoNoPasswd(o.sudoNoPasswdCmdsFastRoot())
-	} else {
-		return o.execCheckIfSudoNoPasswd(o.sudoNoPasswdCmdsDeep())
-	}
-}
-
-func (o *rocky) sudoNoPasswdCmdsFast() []cmd {
-	return []cmd{}
-}
-
-func (o *rocky) sudoNoPasswdCmdsFastRoot() []cmd {
-	if !o.ServerInfo.IsContainer() {
-		return []cmd{
-			{"repoquery -h", exitStatusZero},
-			{"needs-restarting", exitStatusZero},
-			{"which which", exitStatusZero},
-			{"stat /proc/1/exe", exitStatusZero},
-			{"ls -l /proc/1/exe", exitStatusZero},
-			{"cat /proc/1/maps", exitStatusZero},
-			{"lsof -i -P", exitStatusZero},
-		}
-	}
-	return []cmd{
-		{"repoquery -h", exitStatusZero},
-		{"needs-restarting", exitStatusZero},
-	}
-}
-
-func (o *rocky) sudoNoPasswdCmdsDeep() []cmd {
-	return o.sudoNoPasswdCmdsFastRoot()
-}
-
-type rootPrivRocky struct{}
-
-func (o rootPrivRocky) repoquery() bool {
-	return false
-}
-
-func (o rootPrivRocky) yumMakeCache() bool {
-	return false
-}
-
-func (o rootPrivRocky) yumPS() bool {
-	return false
 }
