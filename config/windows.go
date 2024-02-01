9dbdf726760ca7eb6ff3257b5575485554274f7a
wadahiroka <wadahiroka@wadahirokanoMBP.AirPort>
2024-02-01 12:39:02 +0900

    delete: cab validation

--- config/windows.go
+++ config/windows.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"os"
-
 	"golang.org/x/xerrors"
 )
 
@@ -15,11 +13,7 @@ type WindowsConf struct {
 // Validate validates configuration
 func (c *WindowsConf) Validate() []error {
 	switch c.ServerSelection {
-	case 0, 1, 2:
-	case 3:
-		if _, err := os.Stat(c.CabPath); err != nil {
-			return []error{xerrors.Errorf("%s does not exist. err: %w", c.CabPath, err)}
-		}
+	case 0, 1, 2, 3:
 	default:
 		return []error{xerrors.Errorf("ServerSelection: %d does not support . Reference: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-uamg/07e2bfa4-6795-4189-b007-cc50b476181a", c.ServerSelection)}
 	}
