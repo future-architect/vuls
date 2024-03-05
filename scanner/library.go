584a4419ba9b66a057de19c50bcbe96777a9cd6c
TsubasaKanemitsu <is0209xx@gmail.com>
2024-03-05 14:19:29 +0900

    feat(scanner/library): add purl for lockfile scan result

--- scanner/library.go
+++ scanner/library.go
@@ -1,20 +1,26 @@
 package scanner
 
 import (
-	"github.com/aquasecurity/trivy/pkg/fanal/types"
+	ftypes "github.com/aquasecurity/trivy/pkg/fanal/types"
+	"github.com/aquasecurity/trivy/pkg/purl"
+	"github.com/aquasecurity/trivy/pkg/types"
+	"github.com/future-architect/vuls/logging"
 	"github.com/future-architect/vuls/models"
+	"github.com/package-url/packageurl-go"
 )
 
-func convertLibWithScanner(apps []types.Application) ([]models.LibraryScanner, error) {
+func convertLibWithScanner(apps []ftypes.Application) ([]models.LibraryScanner, error) {
 	scanners := []models.LibraryScanner{}
 	for _, app := range apps {
 		libs := []models.Library{}
 		for _, lib := range app.Libraries {
+			purl := newPURL(app.Type, types.Metadata{}, lib)
 			libs = append(libs, models.Library{
-				Name:     lib.Name,
-				Version:  lib.Version,
-				FilePath: lib.FilePath,
-				Digest:   string(lib.Digest),
+				Name:       lib.Name,
+				Version:    lib.Version,
+				PackageURL: purl.ToString(),
+				FilePath:   lib.FilePath,
+				Digest:     string(lib.Digest),
 			})
 		}
 		scanners = append(scanners, models.LibraryScanner{
@@ -24,4 +30,13 @@ func convertLibWithScanner(apps []types.Application) ([]models.LibraryScanner, e
 		})
 	}
 	return scanners, nil
+}
+
+func newPURL(pkgType ftypes.TargetType, metadata types.Metadata, pkg ftypes.Package) *packageurl.PackageURL {
+	p, err := purl.New(pkgType, metadata, pkg)
+	if err != nil {
+		logging.Log.Errorf("Failed to create PackageURL: %+v", err)
+		return nil
+	}
+	return p.Unwrap()
 }
