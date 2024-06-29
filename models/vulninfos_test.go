3f65f521f01fd9a034a4f743817fc3d1c3cf908e
MaineK00n <mainek00n.1229@gmail.com>
2024-06-30 02:26:54 +0900

    feat(cve/nvd): support CVSS v4.0

--- models/vulninfos_test.go
+++ models/vulninfos_test.go
@@ -1931,6 +1931,15 @@ func TestVulnInfo_Cvss40Scores(t *testing.T) {
 							Optional:       map[string]string{"source": "CNA"},
 						},
 					},
+					Nvd: []CveContent{
+						{
+							Type:           Nvd,
+							Cvss40Score:    6.9,
+							Cvss40Vector:   "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:L/VI:L/VA:L/SC:N/SI:N/SA:N/E:X/CR:X/IR:X/AR:X/MAV:X/MAC:X/MAT:X/MPR:X/MUI:X/MVC:X/MVI:X/MVA:X/MSC:X/MSI:X/MSA:X/S:X/AU:X/R:X/V:X/RE:X/U:X",
+							Cvss40Severity: "MEDIUM",
+							Optional:       map[string]string{"source": "cna@vuldb.com"},
+						},
+					},
 				},
 			},
 			want: []CveContentCvss{
@@ -1941,6 +1950,15 @@ func TestVulnInfo_Cvss40Scores(t *testing.T) {
 						Score:    6.9,
 						Severity: "MEDIUM",
 						Vector:   "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:L/VI:L/VA:L/SC:N/SI:N/SA:N",
+					},
+				},
+				{
+					Type: Nvd,
+					Value: Cvss{
+						Type:     CVSS40,
+						Score:    6.9,
+						Severity: "MEDIUM",
+						Vector:   "CVSS:4.0/AV:N/AC:L/AT:N/PR:N/UI:N/VC:L/VI:L/VA:L/SC:N/SI:N/SA:N/E:X/CR:X/IR:X/AR:X/MAV:X/MAC:X/MAT:X/MPR:X/MUI:X/MVC:X/MVI:X/MVA:X/MSC:X/MSI:X/MSA:X/S:X/AU:X/R:X/V:X/RE:X/U:X",
 					},
 				},
 			},
