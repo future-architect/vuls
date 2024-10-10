2c51bcf4764b7e0fd151a1ceb5cde0640a971fb1
MaineK00n <mainek00n.1229@gmail.com>
2024-10-10 12:53:45 +0900

    feat(contrib/snmp2cpe): add --port/-P option (#2046)

--- contrib/snmp2cpe/pkg/cmd/v1/v1.go
+++ contrib/snmp2cpe/pkg/cmd/v1/v1.go
@@ -13,12 +13,14 @@ import (
 
 // SNMPv1Options ...
 type SNMPv1Options struct {
+	Port  uint16
 	Debug bool
 }
 
 // NewCmdV1 ...
 func NewCmdV1() *cobra.Command {
 	opts := &SNMPv1Options{
+		Port:  161,
 		Debug: false,
 	}
 
@@ -28,7 +30,7 @@ func NewCmdV1() *cobra.Command {
 		Example: "$ snmp2cpe v1 192.168.100.1 public",
 		Args:    cobra.ExactArgs(2),
 		RunE: func(_ *cobra.Command, args []string) error {
-			r, err := snmp.Get(gosnmp.Version1, args[0], snmp.WithCommunity(args[1]), snmp.WithDebug(opts.Debug))
+			r, err := snmp.Get(gosnmp.Version1, args[0], snmp.WithCommunity(args[1]), snmp.WithPort(opts.Port), snmp.WithDebug(opts.Debug))
 			if err != nil {
 				return errors.Wrap(err, "failed to snmpget")
 			}
@@ -41,6 +43,7 @@ func NewCmdV1() *cobra.Command {
 		},
 	}
 
+	cmd.Flags().Uint16VarP(&opts.Port, "port", "P", 161, "port")
 	cmd.Flags().BoolVarP(&opts.Debug, "debug", "", false, "debug mode")
 
 	return cmd
