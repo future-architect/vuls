3b796c81643d40fbb5392b5c0e6239cc9b3a2b8b
MaineK00n <mainek00n.1229@gmail.com>
2025-12-15 17:25:46 +0900

    feat(cwe): add 2023, 2024, 2025 top 25 (#2375)

--- detector/detector_test.go
+++ detector/detector_test.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/future-architect/vuls/cwe"
 	"github.com/future-architect/vuls/models"
 	cvemodels "github.com/vulsio/go-cve-dictionary/models"
 )
@@ -129,6 +130,75 @@ func Test_getMaxConfidence(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotMax := getMaxConfidence(tt.args.detail); !reflect.DeepEqual(gotMax, tt.wantMax) {
 				t.Errorf("getMaxConfidence() = %v, want %v", gotMax, tt.wantMax)
+			}
+		})
+	}
+}
+
+func TestFillCweDict(t *testing.T) {
+	type args struct {
+		r *models.ScanResult
+	}
+	tests := []struct {
+		name string
+		args args
+		want models.CweDict
+	}{
+		{
+			name: "happy",
+			args: args{
+				r: &models.ScanResult{
+					ScannedCves: models.VulnInfos{
+						"CVE-2024-49038": models.VulnInfo{
+							CveID: "CVE-2024-49038",
+							CveContents: models.CveContents{
+								models.Mitre: []models.CveContent{
+									{
+										Type:   models.Mitre,
+										CveID:  "CVE-2024-49038",
+										CweIDs: []string{"CWE-79"},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: models.CweDict{
+				"79": models.CweDictEntry{
+					En: &cwe.Cwe{
+						CweID:       "79",
+						Name:        "Improper Neutralization of Input During Web Page Generation ('Cross-site Scripting')",
+						Description: "The software does not neutralize or incorrectly neutralizes user-controllable input before it is placed in output that is used as a web page that is served to other users.",
+						Lang:        "en",
+					},
+					OwaspTopTens: map[string]string{
+						"2017": "7",
+						"2021": "3",
+					},
+					CweTopTwentyfives: map[string]string{
+						"2019": "2",
+						"2020": "1",
+						"2021": "2",
+						"2022": "2",
+						"2023": "2",
+						"2024": "1",
+						"2025": "1",
+					},
+					SansTopTwentyfives: map[string]string{
+						"2010":   "1",
+						"2011":   "4",
+						"latest": "2",
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			FillCweDict(tt.args.r)
+			if !reflect.DeepEqual(tt.args.r.CweDict, tt.want) {
+				t.Errorf("FillCweDict() = %v, want %v", tt.args.r.CweDict, tt.want)
 			}
 		})
 	}
