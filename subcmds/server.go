3ef544c3147c96149496a5e4ad121a4dbfab82d5
Shunichi Shinohara <shino.shun@gmail.com>
2026-03-05 16:13:07 +0900

    remove goval-dictionary related code

--- subcmds/server.go
+++ subcmds/server.go
@@ -120,7 +120,7 @@ func (p *ServerCmd) Execute(_ context.Context, _ *flag.FlagSet, _ ...any) subcom
 	}
 
 	logging.Log.Info("Validating DBs...")
-	if err := detector.ValidateDBs(config.Conf.CveDict, config.Conf.OvalDict, config.Conf.Gost, config.Conf.Exploit, config.Conf.Metasploit, config.Conf.KEVuln, config.Conf.Cti, config.Conf.LogOpts); err != nil {
+	if err := detector.ValidateDBs(config.Conf.CveDict, config.Conf.Gost, config.Conf.Exploit, config.Conf.Metasploit, config.Conf.KEVuln, config.Conf.Cti, config.Conf.LogOpts); err != nil {
 		logging.Log.Errorf("Failed to validate DBs. err: %+v", err)
 		return subcommands.ExitFailure
 	}
